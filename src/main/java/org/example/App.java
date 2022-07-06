afb4f6d84c2871fd0f09bd999b566daad9d395be
jcdominguez <jcd.pro@gmail.com>
2022-07-06 15:53:47 +0200

    Creation d'un JUnit pour exécuter les différents cas

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,15 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        System.out.println( "Hello World!" );
-
-
-        Customer customer = new Customer();
-        customer.setFirstName("Alain");
-        customer.setLastName("Delon");
-
-        CustomerDAO.create(customer);
-
+        System.out.println( "Application CRM" );
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,19 +2,21 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import org.example.dao.CustomerDAO;
+import org.example.entity.Customer;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void createCustomer()
     {
+        Customer customer = new Customer();
+        customer.setFirstName("Alain");
+        customer.setLastName("Delon");
+        CustomerDAO.create(customer);
+
         assertTrue( true );
     }
+
 }
